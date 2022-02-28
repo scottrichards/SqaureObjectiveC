fb42e55fdcdb9118ae27549855a7d17250fdf6aa
Scott Richards <scott.srichards@gmail.com>
2022-02-28 16:47:04 +0100

    Working on drawing rectangle.

--- SquareObjectiveC/ViewController.m
+++ SquareObjectiveC/ViewController.m
@@ -7,14 +7,32 @@
 
 #import "ViewController.h"
 
+
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    [self drawRect:CGRectMake(100.0, 50.0, 500.0, 500.0)];
     // Do any additional setup after loading the view.
 }
 
+//-(void) drawRect:(CGRect)rect{
+////    [super drawRect:rect];
+//    CGRect rectangle = CGRectMake(0, 100, 320, 100);
+////    CGContextRef context = UIGraphicsGetCurrentContext();
+//    CGContextSetRGBFillColor(context, 1.0, 0.0, 0.0, 1.0);
+//    CGContextSetRGBStrokeColor(context, 1.0, 0.0, 0.0, 1.0);
+//    CGContextFillRect(context, rectangle);
+//}
+
+
+- (void)drawRect:(NSRect)rect {
+   [[NSColor redColor] set];
+   CGPathRef path = CGPathCreateMutable();
+   CGPathAddRect(path, NULL, CGRectMake(100, 50, 250, 250));
+   CGPathRelease(path);
+}
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
